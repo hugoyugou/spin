0b6fda374c84e2c44ca1f5aa2b6de830b17dfa5b
HugoYuGou <hugoyugou@gmail.com>
2018-08-04 18:30:45 +0800

    Update Spin Actions: Update, Ready, Play, Pause, Stop, NextFrame

--- src/app/actions/spin.actions.ts
+++ src/app/actions/spin.actions.ts
@@ -1,11 +1,51 @@
 import { Action } from '@ngrx/store';
+import { AnimatedImage } from '../models/AnimatedImage.model';
+import { Frame } from '../models/Frame.model';
 
 export enum SpinActionTypes {
-  LoadSpins = '[Spin] Load Spins'
+  Update = '[Spin] Update',
+  Ready = '[Spin] Ready',
+  Play = '[Spin] Play',
+  Pause = '[Spin] Pause',
+  Stop = '[Spin] Stop',
+  NextFrame = '[Spin] NextFrame',
 }
 
-export class LoadSpins implements Action {
-  readonly type = SpinActionTypes.LoadSpins;
+export class Update implements Action {
+  readonly type = SpinActionTypes.Update;
 }
 
-export type SpinActions = LoadSpins;
+export class Ready implements Action {
+  readonly type = SpinActionTypes.Ready;
+
+  constructor(public payload: ReadyPayload) { }
+}
+
+export class Play implements Action {
+  readonly type = SpinActionTypes.Play;
+}
+
+export class Pause implements Action {
+  readonly type = SpinActionTypes.Pause;
+}
+
+export class Stop implements Action {
+  readonly type = SpinActionTypes.Stop;
+}
+
+export class NextFrame implements Action {
+  readonly type = SpinActionTypes.NextFrame;
+}
+
+export interface ReadyPayload {
+  size: number
+  frame: Frame
+}
+
+export type SpinActions =
+  | Update
+  | Ready
+  | Play
+  | Pause
+  | Stop
+  | NextFrame;
