c91aeb5022aba4d092446ba3cf83f490bdadc645
HugoYuGou <hugoyugou@gmail.com>
2018-08-04 23:27:29 +0800

    Load default image on page load

--- src/app/effects/app.effects.ts
+++ src/app/effects/app.effects.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { mapTo, delay } from 'rxjs/operators';
+import { Update } from '../actions/spin.actions';
 
 
 @Injectable()
 export class AppEffects {
+  
+  @Effect()
+  init$ = this.actions$.pipe(
+    ofType('@ngrx/effects/init'),
+    delay(500),
+    mapTo(new Update())
+  )
 
   constructor(private actions$: Actions) {}
 }
