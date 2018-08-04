52d281a0bd338467b50654161ca4d0621036f5cd
HugoYuGou <hugoyugou@gmail.com>
2018-08-04 14:20:47 +0800

    First design of Gif preview

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Gif } from '../../models/Gif.model';
+import { Frame } from '../../models/Frame.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spin';
+
+  gif: Gif = {
+    current: 0,
+    height: 256,
+    width: 256,
+    frames: [
+      {
+        id: 0,
+        uri: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+        delay: 20,
+        keep: false,
+      } as Frame,
+      {
+        id: 1,
+        uri: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+        delay: 20,
+        keep: false,
+      } as Frame
+    ]
+  } as Gif;
 }
